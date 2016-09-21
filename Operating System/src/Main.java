0c8f7804a02d06f8ebd8b3b359d1e49165e61798
Brandon Watts <brandonwatts@outlook.com>
2016-09-20 23:24:56 -0400

    double gui

--- Operating System/src/Main.java
+++ Operating System/src/Main.java
@@ -1,20 +1,22 @@
 import java.util.function.Consumer;
 
-public class Main {
+public class Main{
 	
 	static Prompt prompt;
 	static Consumer<String> consumer;
 	
     public static void main(String[] args) {
     	
-    	prompt = new Prompt(consumer);
     	InputProcessor input = InputProcessor.getInstance();
     	
          consumer = (String string) -> {
             System.out.println(string);
             input.process(string);
         };
-        
+       
         prompt = new Prompt(consumer);
+        
+     
     }
+
 }
