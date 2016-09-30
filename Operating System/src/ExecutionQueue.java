bd202b32f4ee2919c298c36ae3cdee00f54ddfab
Brandon Watts <brandonwatts@outlook.com>
2016-09-30 19:51:12 -0400

    init ExecutionQueue

---
+++ Operating System/src/ExecutionQueue.java
@@ -0,0 +1,35 @@
+/**Used http://www.gitam.edu/eresource/comp/gvr(os)/4.2.htm as reference **/
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ExecutionQueue {
+	
+	Deque<Process> executionQueue = new LinkedList<Process>();
+	Process.ProcessBuilder head, tail;
+	
+	public ExecutionQueue(Process... processes)
+	{
+		
+		if(processes.length==0)
+			throw new IllegalStateException();
+		
+		for(int i=0; i<processes.length;i++)
+		{
+			this.enQueue(processes[i]);
+		}
+		
+	}
+	
+	public boolean enQueue(Process process)
+	{
+		return executionQueue.add(process);
+	}
+	
+	public void deQueue()
+	{
+		executionQueue.remove();
+	}
+	
+}
