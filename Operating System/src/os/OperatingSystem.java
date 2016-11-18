ce75168cc1c62c0117c55166cd244b5522b19cdd
Brandon Watts <wattsbc2@vcu.edu>
2016-11-17 22:15:05 -0500

    added arrow key functionality and clean up some code

--- Operating System/src/os/OperatingSystem.java
+++ Operating System/src/os/OperatingSystem.java
@@ -1,164 +1,178 @@
 package os;
 
+import os.ui.CommandQueue;
 import os.ui.Prompt;
 import os.ui.TaskManager;
 
 import java.util.ArrayList;
 
 public class OperatingSystem {
-	public static final int MEMORY_SIZE = 256 * 1024;
-	public static final int PAGE_SIZE = 4 * 1024;
-	
-	public static final int NUMBER_OF_REGISTERS = 4;
-	public static final int INSTRUCTION_REGISTER = 0;
-	public static final int PROC_BASE_REGISTER = 1;
-	public static final int PROC_LIMIT_REGISTER = 2;
-	public static final int PROCESS_ID_REGISTER = 3;
-	
-	public static final int QUANTUM = 10;
-	
-	public static Clock clock;
-	public static CPU cpu;
-	public static Dispatcher dispatcher;
-	public static Memory memory;
-	public static Prompt prompt;
-	public static Scheduler scheduler;
-	public static TaskManager taskManager;
 
-	public static void initialize() {
-		clock = new Clock();
-		cpu = new CPU();
-		dispatcher = new Dispatcher();
-		memory = new Memory();
-		prompt = new Prompt();
-		scheduler = new Scheduler();
-		taskManager = new TaskManager();
-	}
-	
-	public static void execute() {
-		scheduler.execute();
-		cpu.execute();
-		clock.execute();
-		System.out.println(cpu.registers[0]);
-	}
-	
-	public static void process(String input) {
-		String[] tokens = input.trim().split("\\s+");
-		
-		if (tokens.length == 0) {
-			return;
-		}
-		
-		switch (tokens[0].toUpperCase()) {
-			case "PROC":
-				//TODO
-				break;
-			
-			case "MEM":
-				//TODO
-				break;
-				
-			case "LOAD":
-				load(tokens);
-				break;
-			
-			case "EXE":
-				exe(tokens);
-				break;
-			
-			case "RESET":
-				prompt.setText(null);
-				break;
-			
-			case "EXIT":
-				System.exit(0);
-				break;
-			
-			case "HELP":
-				prompt.append("The List of availible commands are: proc, mem, load, exe, reset, and exit" + "\n");
-				break;
-				
-			case "TSKMGR":
-				taskManager.setVisible(true);
-				break;
-				
-			default:
-				prompt.append("Error: Not a valid input" + "\n");
-    	}
-	}
-	
-	private static void exe(String[] arguments) {
-		try {
-			// run for a single cycle
-			if (arguments.length == 1) {
-				execute();
-			}
-			
-			// run for the specified number of cycles
-			else if (arguments.length == 2) {
-				String number = arguments[1];
-				int numberOfCycles = 0;
-				
-				try {
-					numberOfCycles = Integer.parseInt(number);
-				} catch (Exception e) {
-					throw new Exception("The number - " + number + " - is incorrect.");
-				}
-				
-				for (int i = 0; i < numberOfCycles; i++) {
-					execute();
-				}
-			}
-			
-			// bad use of exe
-			else {
-				throw new Exception("Incorrect use of exe.");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			prompt.append(e.getMessage() + "\n");
-		}
-	}
-	
-	private static void load(String[] arguments) {
-		try {
-			JobFileData job = null;
+    public static final int MEMORY_SIZE = 256 * 1024;
+    public static final int PAGE_SIZE = 4 * 1024;
 
-			// load in a job file
-			if (arguments.length == 2) {
-				String filename = arguments[1];
-				job = JobFileData.readJobFile(filename);
-			}
-			
-			// load in a program file with a time
-			else if (arguments.length == 3) {
-				String filename = arguments[1];
-				String time = arguments[2];
-				
-				job = new JobFileData();
-				job.programs.add(ProgramFileData.readProgramFile(filename));
-				
-				try {
-					job.startTimes.add(Integer.parseInt(time));
-				} catch (NumberFormatException e) {
-					throw new Exception("The number - " + time + " - is incorrect.");
-				}
-			}
-			
-			// bad use of load
-			else {
-				throw new Exception("Incorrect use of load.");
-			}
-			
-			prompt.append("Files were successfully read in.\n");
-			
-			ArrayList<ProcessData> processes = job.generateProcesses();
-			for (ProcessData process : processes) {
-				scheduler.insertPCB(process);
-			}
-		} catch (Exception e) {
-			prompt.append(e.getMessage() + "\n");
-		}
-		
-	}
+    public static final int NUMBER_OF_REGISTERS = 4;
+    public static final int INSTRUCTION_REGISTER = 0;
+    public static final int PROC_BASE_REGISTER = 1;
+    public static final int PROC_LIMIT_REGISTER = 2;
+    public static final int PROCESS_ID_REGISTER = 3;
+    public static final int NUMBER_OF_SAVED_COMMANDS = 15;
+
+    public static final int QUANTUM = 10;
+
+    public static Clock clock;
+    public static CPU cpu;
+    public static Dispatcher dispatcher;
+    public static Memory memory;
+    public static Prompt prompt;
+    public static Scheduler scheduler;
+    public static TaskManager taskManager;
+    private static CommandQueue<String> commandQueue;
+
+    public static void initialize() {
+        clock = new Clock();
+        cpu = new CPU();
+        dispatcher = new Dispatcher();
+        memory = new Memory();
+        prompt = new Prompt();
+        scheduler = new Scheduler();
+        taskManager = new TaskManager();
+        commandQueue = new CommandQueue<>(NUMBER_OF_SAVED_COMMANDS);
+    }
+
+    public static CommandQueue getCommandQueue() {
+        return commandQueue;
+    }
+
+    public static void execute() {
+        scheduler.execute();
+        cpu.execute();
+        clock.execute();
+        System.out.println(cpu.registers[0]);
+    }
+
+    public static void process(String input) {
+        String[] tokens = input.trim().split("\\s+");
+
+        if (tokens.length == 0) {
+            return;
+        }
+        StringBuilder commandHolder = new StringBuilder();
+        for (String token : tokens) {
+            commandHolder.append(token + " ");
+        }
+        commandQueue.add(commandHolder.toString());
+
+        switch (tokens[0].toUpperCase()) {
+            case "PROC":
+                //TODO
+                break;
+
+            case "MEM":
+                //TODO
+                break;
+
+            case "LOAD":
+                load(tokens);
+                break;
+
+            case "EXE":
+                exe(tokens);
+                break;
+
+            case "RESET":
+                prompt.output(null);
+                break;
+
+            case "EXIT":
+                System.exit(0);
+                break;
+
+            case "HELP":
+                prompt.append("The List of availible commands are: proc, mem, load, exe, reset, and exit" + "\n");
+                break;
+
+            case "TSKMGR":
+                taskManager.setVisible(true);
+                break;
+
+            default:
+                prompt.append("Error: Not a valid input" + "\n");
+        }
+    }
+
+    private static void exe(String[] arguments) {
+        try {
+            // run for a single cycle
+            if (arguments.length == 1) {
+                execute();
+            }
+
+            // run for the specified number of cycles
+            else if (arguments.length == 2) {
+                String number = arguments[1];
+                int numberOfCycles = 0;
+
+                try {
+                    numberOfCycles = Integer.parseInt(number);
+                } catch (Exception e) {
+                    throw new Exception("The number - " + number + " - is incorrect.");
+                }
+
+                for (int i = 0; i < numberOfCycles; i++) {
+                    execute();
+                }
+            }
+
+            // bad use of exe
+            else {
+                throw new Exception("Incorrect use of exe.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            prompt.append(e.getMessage() + "\n");
+        }
+    }
+
+    private static void load(String[] arguments) {
+        try {
+            JobFileData job = null;
+
+            // load in a job file
+            if (arguments.length == 2) {
+                String filename = arguments[1];
+                job = JobFileData.readJobFile(filename);
+            }
+
+            // load in a program file with a time
+            else if (arguments.length == 3) {
+                String filename = arguments[1];
+                String time = arguments[2];
+
+                job = new JobFileData();
+                job.programs.add(ProgramFileData.readProgramFile(filename));
+
+                try {
+                    job.startTimes.add(Integer.parseInt(time));
+                } catch (NumberFormatException e) {
+                    throw new Exception("The number - " + time + " - is incorrect.");
+                }
+            }
+
+            // bad use of load
+            else {
+                throw new Exception("Incorrect use of load.");
+            }
+
+            prompt.append("Files were successfully read in.\n");
+
+            ArrayList<ProcessData> processes = job.generateProcesses();
+            for (ProcessData process : processes) {
+                scheduler.insertPCB(process);
+            }
+        } catch (Exception e) {
+            prompt.append(e.getMessage() + "\n");
+        }
+
+    }
 }
--- Operating System/src/os/ui/Prompt.java
+++ Operating System/src/os/ui/Prompt.java
@@ -2,74 +2,137 @@ package os.ui;
 
 import os.OperatingSystem;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-
-import java.util.function.Consumer;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Prompt {
+
+    int queuePosition = 0;
     private JFrame frame;
-    private JPanel panel;
-    private JTextArea textArea;
-    private JTextField textField;
-    
+    private JTextField commandLine;
+    private Font frameFont;
+    private JTextArea output;
+    final static int MAX_COMMAND_LENGTH = 80;
+    final static int OUTPUT_HEIGHT = 20;
+    final static int OUTPUT_WIDTH = 80;
+    final static int FONT_SIZE = 20;
+
     public Prompt() {
-        panel = new JPanel(new GridBagLayout());
-        Font font = new Font("Consolas", Font.BOLD, 20);
-        
-        textField = new JTextField(80);
-        textField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String string = textField.getText();
-                textArea.append(string + "\n");
-                textField.setText("");
-                OperatingSystem.process(string);
-            }
-        });
-        textField.setFont(font);
-        
-        textArea = new JTextArea(20, 80);
-        textArea.setEditable(false);
-        textArea.setFont(font);
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(textField, c);
-        
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        panel.add(scrollPane, c);
-        
-        frame = new JFrame("Command Prompt");
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setResizable(false);
-        centerWindow(frame);
-        frame.setVisible(true);
+        init();
     }
-    
-    private void centerWindow(JFrame frame) {
+
+    private void centerWindow() {
         Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = defaultToolkit.getScreenSize();
-		int x = (int) ((screenSize.getWidth() / 2) - (frame.getWidth() / 2));
-		int y = (int) ((screenSize.getHeight() / 2) - (frame.getHeight() / 2)); 
+        int x = (int) ((screenSize.getWidth() / 2) - (frame.getWidth() / 2));
+        int y = (int) ((screenSize.getHeight() / 2) - (frame.getHeight() / 2));
         frame.setLocation(x, y);
-	}
-    
-    public void setText(String string) {
-		textArea.setText(string);
-	}
-	
-	public void append(String string) {
-		textArea.append(string);
-	}
-    
+    }
+
+    public void output(final String string) {
+        output.setText(string);
+    }
+
+    final public void writeCommand(final String command) {
+        commandLine.setText(command);
+    }
+
+    public void append(String string) {
+        output.append(string);
+    }
+
+    public void init() {
+        frameFont = new Font("Consolas", Font.BOLD, FONT_SIZE);
+        frame = new JFrame("Command Prompt");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.add(initPanel());
+        frame.pack();
+        centerWindow();
+    }
+
+    /**
+     * Helper method to initialize our JPanel.
+     *
+     * @return JPanel for our Command Prompt
+     */
+    private JPanel initPanel() {
+
+        JPanel panel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(initCommandLine(), constraints);
+
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        panel.add(initOutput(), constraints);
+
+        return panel;
+    }
+
+    /**
+     * Helper method to create the command line and attach listeners.
+     *
+     * @return Command line as a JTextField
+     */
+    private JTextField initCommandLine() {
+        commandLine = new JTextField(MAX_COMMAND_LENGTH);
+        commandLine.setFont(frameFont);
+
+        commandLine.addKeyListener(new KeyListener() {
+            public void keyTyped(final KeyEvent e) {
+            }
+
+            public void keyPressed(final KeyEvent e) {
+                int keyPressed = e.getKeyCode();
+                if (keyPressed == KeyEvent.VK_UP) {
+                    queuePosition++;
+                    if (!OperatingSystem.getCommandQueue().isEmpty()) {
+                        String command = (String) OperatingSystem.getCommandQueue().get(Math.abs(queuePosition) % OperatingSystem.getCommandQueue().size());
+                        OperatingSystem.prompt.writeCommand(command.trim());
+                    }
+                }
+                if (keyPressed == KeyEvent.VK_DOWN) {
+                    if (!OperatingSystem.getCommandQueue().isEmpty()) {
+                        queuePosition--;
+                        String command = (String) OperatingSystem.getCommandQueue().get(Math.abs(queuePosition) % OperatingSystem.getCommandQueue().size());
+                        OperatingSystem.prompt.writeCommand(command.trim());
+                    }
+                }
+            }
+
+            public void keyReleased(final KeyEvent e) {
+            }
+        });
+
+        commandLine.addActionListener(event -> {
+            String string = commandLine.getText();
+            output.append(string + "\n");
+            commandLine.setText("");
+            OperatingSystem.process(string);
+        });
+
+        return commandLine;
+    }
+
+    /**
+     * Helper Method to generate the Output window.
+     *
+     * @return The Output window.
+     */
+    private JScrollPane initOutput() {
+        output = new JTextArea(OUTPUT_HEIGHT, OUTPUT_WIDTH);
+        output.setEditable(false);
+        output.setFont(frameFont);
+        output.setLineWrap(true);
+        output.setWrapStyleWord(true);
+        return new JScrollPane(output);
+    }
+
 }
